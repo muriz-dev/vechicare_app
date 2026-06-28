7efa5f12f33ba7c1e5281707a0bf24adeb66299a
muriz.dev <mhmdrizki.dev@gmail.com>
2026-06-28 19:38:18 +0700

    feat: bottom navigation bar using auto tab scaffold

---
+++ lib/home_page.dart
@@ -0,0 +1,41 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:vechicare_app/core/routes/app_router.dart';
+
+@RoutePage()
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AutoTabsScaffold(
+      routes: [DashboardRoute(), DiagnosisResultRoute(), ProfileRoute()],
+      bottomNavigationBuilder: (_, tabsRouter) {
+        return BottomNavigationBar(
+          selectedItemColor: Theme.of(context).colorScheme.primary,
+          unselectedItemColor: Theme.of(context).colorScheme.onSurfaceVariant,
+          type: BottomNavigationBarType.fixed,
+          currentIndex: tabsRouter.activeIndex,
+          onTap: tabsRouter.setActiveIndex,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.dashboard_outlined),
+              activeIcon: Icon(Icons.dashboard),
+              label: 'Monitoring',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.health_and_safety_outlined),
+              activeIcon: Icon(Icons.health_and_safety),
+              label: 'Diagnosis',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person_outlined),
+              activeIcon: Icon(Icons.person),
+              label: 'Profile',
+            ),
+          ],
+        );
+      },
+    );
+  }
+}

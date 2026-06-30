badfe92b46d10d09e5410a80765221fb80857e9b
muriz.dev <mhmdrizki.dev@gmail.com>
2026-06-30 16:21:30 +0700

    refactor: change vehicle model fields based on new telemetry data

--- lib/data/models/vehicle_model.dart
+++ lib/data/models/vehicle_model.dart
@@ -1,20 +1,20 @@
 class VehicleModel {
   final String vehicleId;
   final String vehicleName;
-  final double speed;
+  final double rpm;
   final double engineTemperature;
+  final double fuelConsumption;
   final int fuelBatteryLevel;
-  final bool isEngineOn;
   final double latitude;
   final double longitude;
 
   VehicleModel({
     required this.vehicleId,
     required this.vehicleName,
-    required this.speed,
+    required this.rpm,
     required this.engineTemperature,
+    required this.fuelConsumption,
     required this.fuelBatteryLevel,
-    required this.isEngineOn,
     required this.latitude,
     required this.longitude,
   });
@@ -23,10 +23,10 @@ class VehicleModel {
     return VehicleModel(
       vehicleId: json['vehicle_id'] as String,
       vehicleName: json['vehicle_name'] as String,
-      speed: (json['speed'] as num).toDouble(),
+      rpm: (json['rpm'] as num).toDouble(),
       engineTemperature: (json['engine_temperature'] as num).toDouble(),
+      fuelConsumption: (json['fuel_consumption'] as num).toDouble(),
       fuelBatteryLevel: json['fuel_battery_level'] as int,
-      isEngineOn: json['is_engine_on'] as bool,
       latitude: (json['latitude'] as num).toDouble(),
       longitude: (json['longitude'] as num).toDouble(),
     );
@@ -35,10 +35,10 @@ class VehicleModel {
   Map<String, dynamic> toJson() => {
     'vehicle_id': vehicleId,
     'vehicle_name': vehicleName,
-    'speed': speed,
+    'rpm': rpm,
     'engine_temperature': engineTemperature,
+    'fuel_consumption': fuelConsumption,
     'fuel_battery_level': fuelBatteryLevel,
-    'is_engine_on': isEngineOn,
     'latitude': latitude,
     'longitude': longitude,
   };
@@ -46,20 +46,20 @@ class VehicleModel {
   VehicleModel copyWith({
     String? vehicleId,
     String? vehicleName,
-    double? speed,
+    double? rpm,
     double? engineTemperature,
+    double? fuelConsumption,
     int? fuelBatteryLevel,
-    bool? isEngineOn,
     double? latitude,
     double? longitude,
   }) {
     return VehicleModel(
       vehicleId: vehicleId ?? this.vehicleId,
       vehicleName: vehicleName ?? this.vehicleName,
-      speed: speed ?? this.speed,
+      rpm: rpm ?? this.rpm,
       engineTemperature: engineTemperature ?? this.engineTemperature,
+      fuelConsumption: fuelConsumption ?? this.fuelConsumption,
       fuelBatteryLevel: fuelBatteryLevel ?? this.fuelBatteryLevel,
-      isEngineOn: isEngineOn ?? this.isEngineOn,
       latitude: latitude ?? this.latitude,
       longitude: longitude ?? this.longitude,
     );
